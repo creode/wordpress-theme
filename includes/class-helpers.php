f17f9ecb11f0af57ce1bfc4de16c640a94918e32
Ben Leonard <ben.leonard@creode.co.uk>
2025-07-30 09:48:36 +0100

    Updated folder duplication functionality to ensure the destination directory exists.

--- includes/class-helpers.php
+++ includes/class-helpers.php
@@ -20,18 +20,21 @@ class Helpers {
 	 * @param bool   $merge (Optional) If true pre-existing destination files will not be overridden. Defaults to true.
 	 */
 	public static function copy_directory( string $source_directory_path, string $destination_directory_path, bool $merge = true ) {
+		// If destination directory doesn't exist, create it.
+		if ( ! is_dir( $destination_directory_path ) ) {
+				// phpcs:ignore WordPress.WP.AlternativeFunctions.file_system_operations_mkdir
+				mkdir( $destination_directory_path );
+		}
+
+		// Loop through files in the source directory.
 		foreach ( scandir( $source_directory_path ) as $file ) {
 			// Bypass references to parent directories.
 			if ( '.' === substr( $file, 0, 1 ) ) {
 				continue;
 			}
 
-			// If a sub-directory is found, ensure an equivalent destination directory exists and recursively copy it's files.
+			// If a sub-directory is found, recursively copy it's files.
 			if ( is_dir( $source_directory_path . '/' . $file ) ) {
-				if ( ! is_dir( $destination_directory_path . '/' . $file ) ) {
-					// phpcs:ignore WordPress.WP.AlternativeFunctions.file_system_operations_mkdir
-					mkdir( $destination_directory_path . '/' . $file );
-				}
 				static::copy_directory( $source_directory_path . '/' . $file, $destination_directory_path . '/' . $file, $merge );
 				continue;
 			}
