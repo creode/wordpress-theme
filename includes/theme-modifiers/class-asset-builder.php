ca3038df04193dc66318bb4b507f8912c6354ee0
Ben Leonard <ben.leonard@creode.co.uk>
2025-08-01 13:06:49 +0100

    Refactor

--- includes/theme-modifiers/class-asset-builder.php
+++ includes/theme-modifiers/class-asset-builder.php
@@ -23,25 +23,33 @@ final class Asset_Builder extends Theme_Modifier_Base {
 	 * Compiles front-end assets.
 	 */
 	private function build() {
-		$output_strings = array();
-
-		exec(
-			implode(
-				' && ',
-				array(
-					'cd ' . get_theme_root() . '/' . $this->theme_directory_name,
-					'npm install',
-					'npm run build',
+		$command = str_replace(
+			'\&\&',
+			'&&',
+			escapeshellcmd(
+				implode(
+					' && ',
+					array(
+						'cd ' . get_theme_root() . '/' . $this->theme_directory_name,
+						'npm install',
+						'npm run build',
+					)
 				)
-			),
-			$output_strings
+			)
 		);
+
+		$output_strings = array();
+		exec( $command, $output_strings );
 
 		if ( is_null( $this->message_handler ) ) {
 			return;
 		}
 
 		foreach ( $output_strings as $output_string ) {
+			if ( empty( $output_string ) ) {
+				continue;
+			}
+
 			$this->message_handler->handle( $output_string );
 		}
 	}
