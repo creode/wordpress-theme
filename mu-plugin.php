e467704cc6f6b702820b2259779c18bfbb2586cc
Ben Leonard <ben.leonard@creode.co.uk>
2025-08-01 14:21:49 +0100

    Integrated creode/mu-plugin-installer

---
+++ mu-plugin.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: WordPress Theme
+ * Description: A theme installation and development framework.
+ * Version: :PLUGIN_VERSION:
+ *
+ * @package Creode Theme
+ */
+
+// This is a loader for the plugin from the parent directory to handle MU Plugin installation.
+require_once __DIR__ . '/wordpress-theme/plugin.php';
