ff260a5d55724b7eb249a102d3c0ee58b74c8bcf
Ben Leonard <ben.leonard@creode.co.uk>
2025-08-27 16:16:55 +0100

    Added functionality to provide stylesheets that the main theme stylesheets are dependent on.

---
+++ theme-template/includes/register-stylesheet-dependencies.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * File for registering stylesheet dependencies. For example third-party font provider integrations.
+ *
+ * @package :THEME_LABEL
+ * @creode-wordpress-theme-version :THEME_PLUGIN_VERSION
+ */
+
+use Creode_Theme\Asset_Enqueue;
+
+// Asset_Enqueue::get_instance()->add_stylesheet_dependency(
+// 	'typekit',
+// 	'https://use.typekit.net/example.css'
+// );
